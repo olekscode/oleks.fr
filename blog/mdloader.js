d7c7d57762e026476a00d9e13ab5b5439f297aa7
Oleksandr Zaytsev <olk.zaytsev@gmail.com>
2020-01-29 00:42:31 +0100

    Implemented the list of posts on the blog

--- blog/mdloader.js
+++ blog/mdloader.js
@@ -12,6 +12,11 @@ function getContentsOfFileFromURL(url) {
   return result;
 }
 
+function getJsonFromURL(url) {
+  jsonString = getContentsOfFileFromURL(url);
+  return JSON.parse(jsonString);
+}
+
 function convertMarkdownToHtml(markdownText) {
   var converter = new showdown.Converter();
   return converter.makeHtml(markdownText);
@@ -23,5 +28,26 @@ function loadMarkdown(url, divId) {
   document.getElementById(divId).innerHTML = html;
 }
 
-url = "https://raw.githubusercontent.com/pharo-open-documentation/pharo-wiki/master/General/Baselines.md";
-loadMarkdown(url, "post");
+function openPost(postId) {
+  url = `https://raw.githubusercontent.com/olekscode/Blog/master/posts/${postId}.md`;
+  loadMarkdown(url, "container");
+}
+
+function openListOfPosts() {
+  url = "https://raw.githubusercontent.com/olekscode/Blog/master/metadata/posts.json";
+  json = getJsonFromURL(url);
+
+  html = '<h1>Published Posts</h1>'
+  listItems = ""
+
+  for (const [key, value] of Object.entries(json)) {
+    listItems += `<li><a href="pleaseEnableJavaScript.html" onClick="openPost('${key}'); return false">${value}</a></li>`;
+  }
+
+  html += `\n<ul>${listItems}</ul>`
+  document.getElementById('container').innerHTML = html;
+  return false;
+}
+
+$('#allPostsButton').click(openListOfPosts);
+openListOfPosts();
