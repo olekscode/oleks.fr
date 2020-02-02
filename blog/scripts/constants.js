8bf020d66dbd31aca092ca852f16c7efc14765a4
olekscode <olk.zaytsev@gmail.com>
2020-02-02 19:06:00 +0100

    Removed blog/, it was moved to blog.oleks.fr

--- blog/scripts/constants.js
+++
@@ -1,7 +0,0 @@
-const blogRawUrl = 'https://raw.githubusercontent.com/olekscode/Blog/master';
-const metadataFolder = blogRawUrl + '/metadata';
-
-export const urls = Object.freeze({
-  postsFolder: blogRawUrl + '/posts',
-  postsMetadataFile: metadataFolder + '/posts.json'
-});
