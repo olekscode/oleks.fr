e9971aa4cb8bfa6c27dea5d02fa941744f83d3bc
Oleksandr Zaitsev <olk.zaytsev@gmail.com>
2023-11-24 22:33:12 +0100

    Added a basic navbar

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -1,43 +1,44 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+
 import './style.css';
 import * as ROUTES from '../../constants/routes';
 
-// import logo from '../../img/logo-small.png';
 
-
-const Navigation = () => (
-  <div id="navbar">
-    <div id="navbar-buttons">
-      <ul>
-{/*        <li>
-          <Link to={ROUTES.LANDING}><img src={logo} alt='' /></Link>
-        </li>*/}
-        <li>
-          <Link className="navbar-button" to={ROUTES.LANDING}>Home</Link>
-        </li>
-        <li>
-          <Link className="navbar-button" to={ROUTES.PUBLICATIONS}>Publications</Link>
-        </li>
-        <li>
-          <Link className="navbar-button" to={ROUTES.SOFTWARE}>Software</Link>
-        </li>
-        <li>
-          <Link className="navbar-button" to={ROUTES.TEACHING}>Teaching</Link>
-        </li>
-        <li>
-          <Link className="navbar-button" to={ROUTES.TALKS}>Talks</Link>
-        </li>
-        <li>
-          <Link className="navbar-button" to={ROUTES.BLOG}>Blog</Link>
-        </li>
-        <li>
-          <Link className="navbar-button" to={ROUTES.CV}>CV</Link>
-        </li>
-      </ul>
-    </div>
-  </div>
-);
-
-export default Navigation;
+export default function Navigation() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Oleksandr Zaitsev
+          </Typography>
+          <Button component={Link} to={ROUTES.LANDING} color="inherit">Home</Button>
+          <Button component={Link} to={ROUTES.PUBLICATIONS} color="inherit">Publications</Button>
+          <Button component={Link} to={ROUTES.SOFTWARE} color="inherit">Software</Button>
+          <Button component={Link} to={ROUTES.TEACHING} color="inherit">Teaching</Button>
+          <Button component={Link} to={ROUTES.TALKS} color="inherit">Talks</Button>
+          <Button component={Link} to={ROUTES.BLOG} color="inherit">Blog</Button>
+          <Button component={Link} to={ROUTES.CV} color="inherit">CV</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
