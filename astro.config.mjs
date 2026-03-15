44972956cc4181d7e92f84c3815932f93b423112
Oleksandr Zaitsev <olk.zaytsev@gmail.com>
2026-03-15 18:55:12 +0100

    Fixed astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,12 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
+// export default defineConfig({
+//   site: 'https://oleks.fr',
+//   integrations: [mdx(), sitemap()]
+// });
+
 export default defineConfig({
-  site: 'https://oleks.fr',
-  integrations: [mdx(), sitemap()]
+  site: 'https://olekscode.github.io/oleks.fr',
+  base: '/oleks.fr/'
 });
